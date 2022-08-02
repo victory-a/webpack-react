dcbcb39773442b91d0c20d07c5534c4e2e34ea5a
victory-a <victoryasokomeh@gmail.com>
2022-08-02 02:16:17 +0100

    final push

---
+++ src/index.js
@@ -0,0 +1,9 @@
+require('file-loader?name=[name].[ext]!./index.html');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { App } from './App';
+import './App.scss';
+
+const root = document.getElementById('root');
+
+ReactDOM.render(<App />, root);
